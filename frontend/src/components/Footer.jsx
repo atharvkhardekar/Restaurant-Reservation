a16ef914f757a25a092b45d1f263a98672b2b398
atharvkhardekar <khardekaratharv@gmail.com>
2024-12-31 21:19:57 +0530

    Frontend Components Completed

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">AK</div>
+          <div className="right">
+            <p>Jalna, Maharashtra</p>
+            <p>Open: 9:00 AM - 11:00 PM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By Atharv Khardekar</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By Atharv Khardekar.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
