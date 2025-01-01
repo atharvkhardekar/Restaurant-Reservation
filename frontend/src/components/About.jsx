7dd4d9dc2be7779ad424b2e0d05241fc6830bdb9
atharvkhardekar <khardekaratharv@gmail.com>
2025-01-01 22:31:35 +0530

    About Section updated

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -13,13 +13,13 @@ const About = () => {
               <p>The only thing we're serious about is food.</p>
             </div>
             <p className="mid">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              Provident possimus optio adipisci dolores impedit illum iusto
-              perferendis, laudantium quod accusamus consequuntur consectetur,
-              tempore nulla error iure reiciendis dolorem assumenda.
-              Necessitatibus fugit asperiores totam rem esse exercitationem
-              iusto ipsum qui dolore ex, accusantium repellat mollitia
-              repellendus.
+            At FOOD4U, we believe every meal tells a story. Nestled in the heart of Jalna, our passion for culinary excellence drives us to create unforgettable dining experiences.
+
+Our journey began with a simple idea: to bring people together through the universal language of food. Each dish on our menu is crafted with love, using the freshest ingredients sourced locally and sustainably. From time-honored recipes to innovative flavors, we offer a perfect blend of tradition and creativity.
+
+More than just a place to eat, FOOD4U is where memories are made. Whether it’s a family gathering, a romantic date night, or a celebration with friends, our warm ambiance and exceptional service will make you feel right at home.
+
+Join us and discover why food is not just our passion—it’s our way of life. Welcome to FOOD4U, where every bite is a promise of happiness.
             </p>
             <Link to={"/"}>
               Explore Menu{" "}
