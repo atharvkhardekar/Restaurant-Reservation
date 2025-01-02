70d78f139d61c3a67dc50f2b7eabd4230363e0c0
atharvkhardekar <khardekaratharv@gmail.com>
2025-01-02 12:09:02 +0530

    Reservation URL updated to render URL

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:5000/api/reservation/send",
+        "https://ak-foodfu-restaurant.onrender.com/api/reservation/send",
         { firstName, lastName, email, phone, date, time },
         {
           headers: {
